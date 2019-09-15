247ba98875074059569a19b42b42a569704241a9
aharonranyvision <43805983+aharonranyvision@users.noreply.github.com>
2019-09-15 15:53:00 +0300

    Async gravity package installer

--- gravity_package_installer.sh
+++ gravity_package_installer.sh
@@ -15,16 +15,16 @@ if [ $PACKAGE_CONTENT ]; then
   printf "Connecting to Gravity Ops Center...\n"
   gravity ops connect --insecure https://localhost:3009 admin Passw0rd123
   printf "Pushing $APP_STRING to Gravity Ops Center (background process)...\n"
-  nohup gravity app import --force --insecure --ops-url=https://localhost:3009 $PACKAGE >> /var/log/gravity_app_import__${REPO_NAME}_${APP_NAME}_${APP_VERSION}.log 2>&1 &
-  #printf "Pulling application from Gravity Ops Center...\n"
-  #gravity app pull --force --insecure --ops-url=https://localhost:3009 $APP_STRING
-  printf "Importing $APP_STRING to local Gravity repository...\n"
-  gravity app import $PACKAGE
-  printf "Exporting $APP_STRING to local Docker registry...\n"
-  gravity exec gravity app export $APP_STRING
-  printf "Executing $APP_STRING install hook...\n"
-  gravity exec gravity app hook $@ --debug $APP_STRING install
-  printf "\n\nDone!\n"
+  nohup gravity app import --force --insecure --ops-url=https://localhost:3009 $PACKAGE >> /var/log/gravity_ops_import__${REPO_NAME}_${APP_NAME}_${APP_VERSION}.log 2>&1 &
+  $( \
+  printf "Importing $APP_STRING to local Gravity repository...\n"       && \
+  gravity app import $PACKAGE                                           && \
+  printf "Exporting $APP_STRING to local Docker registry...\n"          && \
+  gravity exec gravity app export $APP_STRING                           && \
+  printf "Executing $APP_STRING install hook...\n"                      && \
+  gravity exec gravity app hook $@ --debug $APP_STRING install          && \
+  printf "\n\nDone!\n"                                                     \
+  ) >> /var/log/gravity_app_import__${REPO_NAME}_${APP_NAME}_${APP_VERSION}.log 2>&1 &
 else
   PACKAGE_CONTENT=$(timeout 0.3 tar tf $PACKAGE app.yaml 2>/dev/null)
   if [ $PACKAGE_CONTENT ]; then
