f28cdf02068cbc3bb1b2553c8f4dfdf44b340a40
digger18 <digger18@users.noreply.github.com>
2019-09-22 08:58:38 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-set -e
+#set -e
 
 # Absolute path to this script
 SCRIPT=$(readlink -f "$0")
 # Absolute path to the script directory
 BASEDIR=$(dirname "$SCRIPT")
-INSTALL_MODE="aio"
-INSTALL_METHOD="online"
-PRODUCT_NAME="bettertomorrow"
-PRODUCT_VERSION="1.23.1-5"
 
 
 ## Permissions check
@@ -49,15 +45,13 @@ function showhelp {
 function backup_secrets {
   if [ -x "$(command -v kubectl)" ]; then
     mkdir -p /opt/backup/secrets
-    echo "###########################"
-    echo "# Backing up secrets. . . #"
-    echo "###########################"
-    secrets_list=$(kubectl get secret | tail -n +2 | awk '{print $1}')
+    echo "#### Backing up Kubernetes secrets..."
+    secrets_list=$(kubectl get secrets | tail -n +2 | awk '{print $1}')
     relevant_secrets_list=("redis-secret" "mongodb-secret" "rabbitmq-secret" "ingress-basic-auth-secret")
     for secret in $secrets_list
     do
       if [[ " ${relevant_secrets_list[@]} " =~ " ${secret} " ]]; then
-        echo "Backing up secret $secret"
+        echo "#### Backing up secret $secret"
         kubectl get secret $secret -o yaml | grep -v "^  \(creation\|resourceVersion\|selfLink\|uid\)" > /opt/backup/secrets/$secret.yaml
       fi
     done
@@ -71,9 +65,7 @@ function remove_nvidia_drivers {
     if dpkg-query --show nvidia-driver-410 ; then
       echo "Nvidia driver is already on the right version (410)"
     else
-      echo "###############################"
-      echo "# Removing Nvidia driver. . . #"
-      echo "###############################"
+      echo "#### Removing Nvidia driver..."
       apt remove -y --purge nvidia-* cuda-*
       apt autoremove
       add-apt-repository -y --remove ppa:graphics-drivers/ppa
@@ -82,65 +74,61 @@ function remove_nvidia_drivers {
     if [ $(modinfo nvidia -F version) == "410.104" ]; then
       echo "Nvidia driver is already on the right version (410)"
     else
-      echo "###############################"
-      echo "# Removing Nvidia driver. . . #"
-      echo "###############################"
-      ./$(ls -la | grep NVIDIA-Linux | awk '{print $NF}') --silent --no-install-compat32-libs --uninstall
+      echo "#### Removing Nvidia driver..."
+      ./$(ls -la | grep NVIDIA-Linux | awk '{print $NF}') --silent --uninstall
     fi
   fi
 }
 
 function remove_nvidia_docker {
-  if [ -x "$(command -v apt-get)" ]; then
-    if  [ -x "$(command -v nvidia-docker)" ]; then
-      echo "###############################"
-      echo "# Removing Nvidia-Docker. . . #"
-      echo "###############################"
+  if [ -x "$(command -v nvidia-docker)" ]; then
+    echo "#### Removing Nvidia-Docker..."
+    if [ -x "$(command -v apt-get)" ]; then
       apt remove -y --purge nvidia-docker*
       apt autoremove
+    elif [ -x "$(command -v yum)" ]; then
+      yum remove -y nvidia-docker*
     fi
-  elif [ -x "$(command -v yum)" ]; then
-      echo "###############################"
-      echo "# Removing Nvidia-Docker. . . #"
-      echo "###############################"
-      yum remove -y nvidia*
+  else
+    echo "#### nvidia-docker does not exists, skipping nvidia-docker removal phase."
   fi
 }
 
 function disable_k3s {
-  if [ -x "$(command -v k3s-uninstall.sh)" ]; then
-    echo "###################################"
-    echo "# Stopping and disabling K3S. . . #"
-    echo "###################################"
-    systemctl is-active --quiet k3s && systemctl stop k3s
-    systemctl is-enabled --quiet k3s && systemctl disable k3s
-    k3s-uninstall.sh
+  if systemctl is-active --quiet k3s; then
+    echo "#### Stopping k3s service..."
+    systemctl stop k3s
+    systemctl is-enabled --quiet k3s && echo "#### Disabling k3s service..." && systemctl disable k3s
   else
-    echo "#### k3s does not exists, skipping k3s removal phase."
+    echo "#### k3s is not active, skipping k3s service disabling phase."
   fi
+  #if [ -x "$(command -v k3s-uninstall.sh)" ]; then
+  #  k3s-uninstall.sh
+  #else
+  #  echo "#### k3s uninstall script does not exists, skipping k3s removal phase."
+  #fi
 }
 
 function disable_docker {
-  if [ -x "$(command -v docker)" ]; then
-    #echo "############################################"
-    #echo "# Killing and removing all containers. . . #"
-    #echo "############################################"
-    #docker kill $(docker ps -q)
-    #docker rm $(docker ps -aq)
+  if [ -x "$(command -v docker)" ] && systemctl is-active --quiet docker; then
+    echo "#### Killing all running containers..."
+    docker kill $(docker ps -q)
+    echo "#### Removing all stopped containers..."
+    docker rm $(docker ps -aq)
+    echo "#### Pruning all docker networks..."
+    docker network prune -f
     #docker system prune -f
-    echo "######################################"
-    echo "# Stopping and disabling Docker. . . #"
-    echo "######################################"
-    systemctl is-active --quiet docker && systemctl stop docker
-    systemctl is-enabled --quiet docker && systemctl disable docker
-    #if [ -x "$(command -v apt-get)" ]; then
-    #  apt remove -y --purge docker*
-    #elif [ -x "$(command -v yum)" ]; then
-    #  yum remove -y docker*
-    #fi
+    echo "#### Stopping Docker service..."
+    systemctl stop docker
+    systemctl is-enabled --quiet docker && echo "#### Disabling Docker service..." && systemctl disable docker
   else
-    echo "#### docker does not exists, skipping docker service disabling phase."
+    echo "#### docker does not exists or is disabled, skipping docker service disabling phase."
   fi
+  #if [ -x "$(command -v docker)" ] && [ -x "$(command -v apt-get)" ]; then
+  #  apt remove -y --purge docker*
+  #elif [ -x "$(command -v docker)" ] && [ -x "$(command -v yum)" ]; then
+  #  yum remove -y docker*
+  #fi
 }
 
 POSITIONAL=()
@@ -155,7 +143,7 @@ while test $# -gt 0; do
         backup_secrets
         disable_k3s
         disable_docker
-        remove_nvidia_docker
+        #remove_nvidia_docker
         remove_nvidia_drivers
         exit 0
         ;;
