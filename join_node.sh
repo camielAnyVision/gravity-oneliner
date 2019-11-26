83de0bac474ec8e7dd74749450fe2754e5fb417c
aharon <aharonr@anyvision.co>
2019-11-26 17:15:56 +0200

    join-node: added script for node join to cluster
    steps are:
    1. run this script on master
    2. run the output on the node for joining node to cluster

---
+++ join_node.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Getting Master Join Token"
+JOIN_TOKEN=$(gravity exec gravity status --token)
+echo "Getting Master IP Address"
+MASTER_IP=$(gravity exec gravity status --output=json | jq -r .cluster.nodes[0].advertise_ip)
+
+echo ""
+echo "RUN the following commands on the node as root to join node to cluster"
+echo ""
+echo ""
+echo "==================================================================="
+echo ""
+echo "curl -k -H \"Authorization: Bearer ${JOIN_TOKEN}\" https://${MASTER_IP}:32009/portal/v1/gravity -o /usr/local/bin/gravity"
+echo chmod +x /usr/local/bin/gravity
+echo gravity join ${MASTER_IP} --token=${JOIN_TOKEN} --role=edge
+echo ""
+echo "====================================================================="
