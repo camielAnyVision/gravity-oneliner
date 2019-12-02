6fa150bb3db74a1f70fcfca2a03917915afae773
digger18 <digger18@users.noreply.github.com>
2019-12-02 19:00:58 +0200

    Update join_node.sh

--- join_node.sh
+++ join_node.sh
@@ -13,6 +13,6 @@ echo "==================================================================="
 echo ""
 echo "curl -k -H \"Authorization: Bearer ${JOIN_TOKEN}\" https://${MASTER_IP}:32009/portal/v1/gravity -o /usr/local/bin/gravity"
 echo chmod +x /usr/local/bin/gravity
-echo gravity join ${MASTER_IP} --token=${JOIN_TOKEN} --role=edge
+echo gravity join ${MASTER_IP} --token=${JOIN_TOKEN} --role=edge --cloud-provider=generic
 echo ""
 echo "====================================================================="
